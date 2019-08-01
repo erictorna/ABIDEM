fd770006983bf1612c0de2d9dc5117284bca8f6f
mcomas <mcomas@idiapjgol.info>
2019-08-01 12:04:32 +0200

    variables reordered

--- imputation/03a-mi-dataset.R
+++ imputation/03a-mi-dataset.R
@@ -11,8 +11,10 @@ library(survival)
 aalen = survfit(coxph(Surv(time, event)~1, data=maria.imp), type='aalen')
 maria.imp$na.event = approx(aalen$time, -log(aalen$surv), xout=as.numeric(maria.imp$time))$y
 
-vrs <- c("ocip", "age", "itb", "weight", "height", "ln_sbp", "ln_pp","ln_glu", "hba1c", "p.hf", "p.diabetes", "p.cvd", "p.b01",
-        "ln_coltot",  "ln_colhdl", "ln_colldl",  "ln_tg", "men", "p.smoking", "p.htn", "p.dyslipidemia", 
+vrs <- c("ocip", "age", "itb", "weight", "height", "ln_sbp", "ln_pp","ln_glu", "hba1c", 
+         "p.hf", "p.diabetes", "p.cvd", "p.b01",
+         "ln_coltot",  "ln_colhdl", "ln_colldl",  "ln_tg", "men", "p.smoking", "p.htn", 
+         "p.dyslipidemia", 
          "p.arthritis", "p.asthma", "p.copd", "p.aff", "p.neoplasms_malignant", "p.ckd",  "p.cvd",
          "p.hypothyroidism", "p.ami", "p.angor", "p.tia", "p.stroke", 
          "p.a10", "p.c02", "p.c03", "p.c07", "p.c08",
