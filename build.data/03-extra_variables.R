f5d1cab5a1ef8fbc8ccec423280531aa915730f1
mcomas <mcomas@idiapjgol.info>
2019-08-01 12:03:47 +0200

    b01a variable included

--- build.data/03-extra_variables.R
+++ build.data/03-extra_variables.R
@@ -17,9 +17,21 @@ b01 = bind_rows(b01_2012, b01_2013) %>%
   filter(dintro - years(1) <= date) %>%
   count(ocip)
 
+b01a = bind_rows(b01_2012, b01_2013) %>%
+  select(ocip = X1, date = X2, atc = X3) %>%
+  filter(ocip %in% maria[['ocip']]) %>%
+  filter(grepl("^B01A", atc)) %>%
+  mutate(date = ymd(paste0(date,'28'))) %>%
+  left_join(maria %>% select(ocip, dintro)) %>%
+  filter(date <= dintro) %>%
+  filter(dintro - years(1) <= date) %>%
+  count(ocip)
+
 maria = maria %>%
   mutate(
-    p.b01 = as.integer(ocip %in% b01$ocip)
+    p.b01 = as.integer(ocip %in% b01$ocip),
+    p.b01a = as.integer(ocip %in% b01a$ocip)
   )
+
 
 save(maria, file = 'data/03-extra_variables.RData')
