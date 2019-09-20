b90de1e4afe8351612015a840e3f42832919d091
mcomas <mcomas@idiapjgol.info>
2019-09-20 16:50:00 +0200

    Es modifica la taula 1 a la nova cohort (cohort sense secundària)

--- tables/table01.R
+++ tables/table01.R
@@ -5,54 +5,47 @@ library(officer)
 source('R/global.R')
 
 GROUPS_LABELS = c('CVD-no_DM2-no' = 'No diabetes', 
-                  'CVD-no_DM2-yes' = 'Diabetes', 
-                  'CVD-yes_DM2-no' = 'No diabetes', 
-                  'CVD-yes_DM2-yes' = 'Diabetes')
+                  'CVD-no_DM2-yes' = 'Diabetes')
 GROUPS = names(GROUPS_LABELS)
-Ns = sapply(GROUPS, function(GROUP){
-  load(sprintf('tables/baseline-characteristics_%s.RData', GROUP))
-  global.raw$numeric %>% 
-    filter(variable == 'age') %>% 
-    pull(n)
-})
-for(group in GROUPS){
-  GROUPS_LABELS[group] = paste(GROUPS_LABELS[group], sprintf("n = %d", Ns[group]))
-}
 
-get_values = function(type, .variable, m_t = identity, s_t = m_t){
-  sapply(GROUPS, get_values_, type, .variable, m_t = identity, s_t = m_t)
-}
+GROUP = 'CVD-no_DM2-yes'
+
 get_values_ = function(GROUP, type, .variable, m_t = identity, s_t = m_t){
   load(sprintf('tables/baseline-characteristics_%s.RData', GROUP))
   if(type == 'numeric'){
-    row = global.imp[[type]] %>%
+    row = itb_cat.imp[[type]] %>%
+      ungroup() %>%
       filter(variable == .variable) %>%
       transmute(
+        itb_cat,
         text = sprintf("%0.2f (%.2f)", m_t(mean), s_t(sd))
       ) %>% 
+      pivot_wider(names_from = itb_cat, values_from = text) %>%
       unlist()
   }
   if(type == 'dichotomic'){
-    row = global.imp[[type]] %>%
+    row = itb_cat.imp[[type]] %>%
       filter(variable == .variable) %>%
+      ungroup() %>%
       transmute(
+        itb_cat,
         text = sprintf("%d (%.1f%%)", m, 100 * p)
       ) %>% 
+      pivot_wider(names_from = itb_cat, values_from = text) %>%
       unlist()
   }
   row
 }
-
-# From mmol/mol to %
-# (New number/10.929)+2.15
-# 
-# From % to mmol/mol
-# (Old number-2.15)x10.929
+get_values = function(type, .variable, m_t = identity, s_t = m_t){
+  unname(get_values_(GROUP, type, .variable, m_t = identity, s_t = m_t))
+}
+K = 6
 info = list(#'N' = as.character(Ns),
   'ABI' = get_values('numeric', 'itb'),
   'Age, years' = get_values('numeric', 'age'),
   'Men' = get_values('dichotomic', 'men'),
   'Smoking habit' = get_values('dichotomic', 'p.smoking'),
+  ## Alcohol
   'Height, cm' = get_values('numeric', 'height'),
   'Weight, kg' = get_values('numeric', 'weight'),
   'BMI, kg/m²' = get_values('numeric', 'bmi'),
@@ -72,7 +65,7 @@ info = list(#'N' = as.character(Ns),
   # 'HbA1c, mmol/mol' = get_values('numeric', 'hba1c', m_t = function(x) (x-2.15)*10.929, s_t = function(x) x * 10.929),
   # 'Glucose, mmol/L' = get_values('numeric', 'glu', function(x) x * 0.0555),
   'Diabetes duration' = get_values('numeric', 'time_diab'),
-  'Comorbidities' = rep('Comorbidities', 6),
+  'Comorbidities' = rep('Comorbidities', K),
   'Hypertension' = get_values('dichotomic', 'p.htn'),
   'Atrial fibrillation' = get_values('dichotomic', 'p.aff'),
   'Heart failure' = get_values('dichotomic', 'p.hf'),
@@ -83,63 +76,65 @@ info = list(#'N' = as.character(Ns),
   # 'Nephropathy' = get_values('dichotomic', 'p.nephro_ckd'),
   # 'Retinopathy' = get_values('dichotomic', 'p.retinopathy'),
   # 'Neuropathy' = get_values('dichotomic', 'p.neuropathy'),
-  'Medication'  = rep('Medication', 6),
+  'Medications'  = rep('Medications', K),
   'Antidiabetic therapy' = get_values('dichotomic', 'p.a10'),
+  'Vitamin K antagonists' = get_values('dichotomic', 'p.b01aa'),
+  'Heparin group' = get_values('dichotomic', 'p.b01ab'),
+  'Other platelet aggregation inhibitors*' = get_values('dichotomic', 'p.b01ac'),
+  'Other antithrombotic agents' = get_values('dichotomic', 'p.b01a_other'),
   'Diuretics' = get_values('dichotomic', 'p.c03'),
   'Beta-blockers' = get_values('dichotomic', 'p.c07'),
   'Calcium channel blockers' = get_values('dichotomic', 'p.c08'),
   'Agents acting on the \nrenin-angiotensin system' = get_values('dichotomic', 'p.c09'),
   'Other antihypertensives' = get_values('dichotomic', 'p.c02'),
   'Statins' = get_values('dichotomic', 'p.statin'),
-  'Other lipid-lowering agent' = get_values('dichotomic', 'p.c10nostatin'),
-  'Antithrombotic Agent'  = rep('Antithrombotic Agent', 6),
-  'Vitamin K antagonists' = get_values('dichotomic', 'p.b01aa'),
-  'Heparin group' = get_values('dichotomic', 'p.b01ab'),
-  'Platelet aggregation inhibitors excl. heparin' = get_values('dichotomic', 'p.b01ac'),
-  'Other antithrombotic agents' = get_values('dichotomic', 'p.b01a_other')
+  'Other lipid-lowering agent' = get_values('dichotomic', 'p.c10nostatin')
 )
-K = length(info[[1]])
 
-tab = tibble(
-  names(info)) %>%
-  bind_cols(as_tibble(lapply(1:K, function(i) sapply(info, nth, i)), .name_repair = 'unique')) 
+load(sprintf('tables/baseline-characteristics_%s.RData', GROUP))
+Ns = itb_cat.imp$numeric %>% 
+  filter(variable == 'age') %>% 
+  ungroup() %>%
+  select(itb_cat, n) %>%
+  pivot_wider(names_from = itb_cat, values_from = n)
+
+tab = as_tibble(do.call('rbind', info), 
+                rownames = "variable", .name_repair = ~make.names(1:length(Ns)))
 
 typology = tibble(
   col_keys = names(tab),
-  lab1 = c('', rep(c(sprintf("No previous CVD\nn = %d", Ns['CVD-no_DM2-no'] + Ns['CVD-no_DM2-yes']),
-                     sprintf("Previous CVD\nn = %d", Ns['CVD-yes_DM2-no'] + Ns['CVD-yes_DM2-yes'])), c(2,2))),
-  lab2 = c('', paste(rep(c('No diabetes', 'Diabetes'), 2), sprintf("n = %d", Ns), sep='\n')))
+  lab1 = c('Variable', '0.4≤ABI<0.5', '0.5≤ABI<0.7', '0.7≤ABI<0.9', '0.9≤ABI<1.1', '1.1≤ABI<1.3', '1.3≤ABI<3'),
+  lab2 = c('', sprintf("n = %d", unlist(Ns))))
 
-
-SUBTITLE = match(c('Comorbidities', 'Medication', 'Antithrombotic Agent'), names(info))
+SUBTITLE = match(c('Comorbidities', 'Medications'), names(info))
+FONT.SIZE = 8
 tbl = tab %>%
   flextable(col_keys = names(tab)) %>%
   set_header_df(mapping = typology, key = "col_keys" ) %>%
-  style(part='header', pr_p = fp_par(text.align = "center")) %>%
-  bold(part = 'header') %>%
-  merge_h(i = 1, part = 'header') %>%
-  add_footer() %>%
-  autofit() %>%
-  height_all(0.25) %>%
-  width(j = 1, 2) %>%
-  width(j = 1+1:K, 1.1) %>%
+  style(pr_t = fp_text(font.size = FONT.SIZE, bold = TRUE), pr_p = fp_par(text.align = "center"), part = 'header') %>%
+  style(j = 1, pr_p = fp_par(text.align = "left"), part = 'header') %>%
+  style(pr_t = fp_text(font.size = FONT.SIZE), pr_p = fp_par(text.align = "right")) %>%
+  style(j = 1, pr_t = fp_text(font.size = FONT.SIZE, bold = TRUE), pr_p = fp_par(padding.left = 6, text.align = "left")) %>%
+  style(j = 1, i = c(1:min(SUBTITLE), SUBTITLE), pr_t = fp_text(font.size = FONT.SIZE, bold = TRUE), pr_p = fp_par(padding.left = 0, text.align = "left")) %>%
   merge_h(i = SUBTITLE) %>%
-  style(i = SUBTITLE, pr_t = fp_text(bold = TRUE, font.size = 11), 
-        pr_p = fp_par(text.align = "left", padding.left = 100, padding.bottom = 0, padding.top = 12)) %>%
-  #style(i = 21, pr_t = fp_text(bold = TRUE, font.size = 11), pr_p = fp_par(text.align = "left", padding.left = 100, padding.bottom = 0, padding.top = 10)) %>%
-  bold(j = 1)
-tbl  
+  border_remove() %>%
+  autofit() %>%
+  width(j = 1, 1.5)
+tbl
 
-heading = fpar(ftext("Table 1. Baseline Characteristics of the Study Population.",
-                     fp_text(font.size = 11, bold = TRUE)))
-footer1 = fpar(ftext("Values are presented as mean (SD) or N(%).", 
-                     fp_text(font.size = 11, bold = FALSE)))
-footer2 = fpar(ftext("ABI indicates ankle brachial index; BMI, body mass index; BP, blood pressure; COPD, chronic obstructive pulmonary disease; CKD, chronic kidney disease, HbA1c, glycated hemoglobin A; HDL, high density lipoprotein; LDL, low density lipoprotein; n, number of participants.", 
-                     fp_text(font.size = 11, bold = FALSE)))
+heading = fpar(ftext(sprintf("Table 1. Baseline characteristics of the study population with diabetes (n = %d)", sum(Ns)),
+                     fp_text(font.size = FONT.SIZE, bold = TRUE)))
+footer1 = fpar(ftext("Values are presented as mean (SD) or n (%).", fp_text(font.size = FONT.SIZE, bold = FALSE)))
+footer2 = fpar(ftext("* Excluding heparin.", fp_text(font.size = FONT.SIZE, bold = FALSE)))
+footer3 = fpar(ftext("ABI indicates ankle brachial index; BP, blood pressure; COPD, chronic obstructive pulmonary disease; CKD, chronic kidney disease, HbA1c, glycated hemoglobin A; n, number of participants.", fp_text(font.size = FONT.SIZE, bold = FALSE)))
+
 read_docx() %>%
   body_add_fpar(value = heading) %>%
+  body_add_fpar(value = fpar()) %>%
   body_add_flextable(value = tbl, align = "left") %>%
-  body_end_section_landscape() %>%
+  #body_end_section_landscape() %>%
   body_add_fpar(value = footer1) %>%
-  body_add_fpar(value = footer2) %>%  
-  print(target = "www/table01_previous.docx")
+  body_add_fpar(value = footer2) %>%
+  body_add_fpar(value = footer3) %>%  
+  print(target = "www/table01.docx")
+
